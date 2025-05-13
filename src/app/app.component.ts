be9e3f7447dcfba09dc13bac401441e83a7c5282
joshaughnessy22 <js973521@ohio.edu>
2025-05-13 11:39:28 -0400

    cards working, need styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,16 @@ import { RouterOutlet } from '@angular/router';
 import { CardsComponent } from './cards/cards.component';
 import { ColorsComponent } from './colors/colors.component';
 import { FontComponent } from './font/font.component';
+import { ErrorComponent } from './error/error.component';
+import { NgIf, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, 
     CardsComponent,
     ColorsComponent,
-    FontComponent
+    FontComponent,
+    ErrorComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,11 +1,69 @@
 import { Component } from '@angular/core';
+import { ErrorComponent } from '../error/error.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
+import { NgIf, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-cards',
-  imports: [],
+  imports: [ MatGridListModule,
+    MatButtonModule,
+    ErrorComponent,
+    NgIf, NgFor
+  ],
   templateUrl: './cards.component.html',
   styleUrl: './cards.component.css'
 })
 export class CardsComponent {
+
+  colors = ['#FFFFFF', '#000000'];
+  errorMessage = '';
+  gridCols = 2;
+  gridRows = 1;
+  gridHeight = 200;
+  gridWidth = 200;
+
+  addGridItem() {
+    if (this.colors.length < 11){
+      this.gridCols = Math.ceil(this.colors.length / 2);
+      this.gridRows = Math.ceil(this.colors.length / this.gridCols);
+      this.gridHeight = 200 * this.gridRows;
+      this.gridWidth = 200 * this.gridCols;
+
+      this.errorMessage = '';
+
+      this.colors.push(this.getRandomColor());
+    }
+    else {
+      this.errorMessage = "You can only add 10 colors";
+    }
+
+  }
+
+  removeGridItem(i: number){
+    if(this.colors.length > 2){
+      this.gridCols = Math.ceil(this.colors.length / 2);
+      this.gridRows = Math.ceil(this.colors.length / this.gridCols);
+      this.gridHeight = 200 * this.gridRows;
+      this.gridWidth = 200 * this.gridCols;
+
+      this.errorMessage = '';
+
+      this.colors.splice(i, 1);
+    } 
+    else {
+      this.errorMessage = "You must have at least 2 colors";
+    }
+  }
+
+  private getRandomColor(): string {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
 
 }
